e97d6e99cb469d51be7cabe74802ddb14515fc83
wildanicardi <wildanicardi9@gmail.com>
2019-07-28 13:01:32 +0700

    fitur delete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 (function () {
+  const list = document.getElementById("list");
   //check fields and hide submit
   document.addEventListener('DOMContentLoaded', function () {
     const display = new Display();
@@ -13,14 +14,22 @@
     const course = this.querySelector('.course');
     const author = this.querySelector('.author');
 
-    const customer = new Customer(name.value,course.value,author.value)
+    const customer = new Customer(name.value, course.value, author.value)
     // console.log(customer);
-    
+
     const display = new Display();
 
     display.feedback(customer);
     display.clearFields();
   });
+  //delete
+  list.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.classList.contains('delete-flashcard')) {
+      // console.log(event.target.parentElement.parentElement);
+      list.removeChild(event.target.parentElement.parentElement);
+    }
+  })
   //display
   function Display() {
     this.name = document.getElementById("name");
@@ -58,38 +67,38 @@
     const btn = document.querySelector(".submitBtn");
     btn.disabled = true;
   };
-  Display.prototype.clearFields = function(){
+  Display.prototype.clearFields = function () {
     // console.log(this);
-    
-    this.name.value ='';
-    this.course.value ='';
-    this.author.value ='';
 
-    this.name.classList.remove('complete','fail');
-    this.course.classList.remove('complete','fail');
-    this.author.classList.remove('complete','fail');
+    this.name.value = '';
+    this.course.value = '';
+    this.author.value = '';
+
+    this.name.classList.remove('complete', 'fail');
+    this.course.classList.remove('complete', 'fail');
+    this.author.classList.remove('complete', 'fail');
   };
   //show loading and feedback
-  Display.prototype.feedback = function(customer){
+  Display.prototype.feedback = function (customer) {
     const feedback = document.querySelector('.feedback');
     const loading = document.querySelector('.loading');
-    feedback.classList.add('showItem','alert','alert-success');
+    feedback.classList.add('showItem', 'alert', 'alert-success');
     loading.classList.add('showItem');
 
     const self = this;
     self.hideSubmit();
     setTimeout(function () {
-      feedback.classList.remove('showItem','alert','alert-success');
+      feedback.classList.remove('showItem', 'alert', 'alert-success');
       loading.classList.remove('showItem');
       self.addCustomer(customer)
-    },4000)
+    }, 4000)
   };
-  Display.prototype.addCustomer = function(customer) {
+  Display.prototype.addCustomer = function (customer) {
     // console.log(customer.name);
 
     const random = this.getRandom();
     const div = document.createElement('div');
-    div.classList.add("col-11","mx-auto","col-md-6","col-lg-4","my-3");
+    div.classList.add("col-11", "mx-auto", "col-md-6", "col-lg-4", "my-3");
     div.innerHTML = `
     <div class="card text-left">
        <img src="img/cust-${random}.jpg" class="card-img-top" alt="">
@@ -107,16 +116,21 @@
         <!-- end of customer name -->
        </div>
       </div>
+      <div class="flashcard-btn d-flex justify-content-between">
+
+        <a href="#" id="edit-flashcard" class=" btn my-1 btn-success edit-flashcard text-uppercase" data-id="">edit</a>
+        <a href="#" id="delete-flashcard" class=" btn my-1 delete-flashcard text-uppercase">delete</a>
+       </div>
     `;
     this.customers.appendChild(div);
   };
   //random number
   Display.prototype.getRandom = function () {
-    let random = Math.floor(Math.random()*5+1);
+    let random = Math.floor(Math.random() * 5 + 1);
     return random;
   };
   //customer constructor
-  function Customer(name,course,author){
+  function Customer(name, course, author) {
     this.name = name;
     this.course = course;
     this.author = author;
